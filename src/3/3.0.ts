a5fe5f33c41d996f1cafc8432c42c912a6ee757b
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-12-03 11:16:41 +0100

    chore: format lint, remove useless log

--- src/3/3.0.ts
+++ src/3/3.0.ts
@@ -1,28 +1,23 @@
-import * as fs from "node:fs";
+import * as fs from 'node:fs';
 
-const file = fs.readFileSync("src/3/input.txt", "utf8");
+const file = fs.readFileSync('src/3/input.txt', 'utf8');
 const nums: number[][] = file
-  .split("\n")
-  .map((lines) => lines.split("").map((x) => parseInt(x, 10)));
+    .split('\n')
+    .map((lines) => lines.split('').map((x) => parseInt(x, 10)));
 
 const maxs = nums.map((line) =>
-  line.slice(0, -1).reduce((pV, aV) => (pV > aV ? pV : aV))
+    line.slice(0, -1).reduce((pV, aV) => (pV > aV ? pV : aV)),
 );
 
 const maxsPos = nums.map((line, index) => line.indexOf(maxs[index] as number));
 
 const numsRemaing = nums.map((line, index) =>
-  line.slice((maxsPos[index] as number) + 1)
+    line.slice((maxsPos[index] as number) + 1),
 );
 
 const maxs2 = numsRemaing.map((line) =>
-  line.reduce((pV, aV) => (pV > aV ? pV : aV))
+    line.reduce((pV, aV) => (pV > aV ? pV : aV)),
 );
-console.log(nums);
-console.log(maxs);
-console.log(maxsPos);
-console.log(numsRemaing);
-console.log(maxs2);
 
 const res = maxs.map((el, i) => el * 10 + (maxs2[i] as number));
 console.log(res.reduce((pV, cV) => cV + pV));
--- src/3/3.1.ts
+++ src/3/3.1.ts
@@ -1,37 +1,28 @@
-import * as fs from "node:fs";
+import * as fs from 'node:fs';
 
-const file = fs.readFileSync("src/3/input.txt", "utf8");
+const file = fs.readFileSync('src/3/input.txt', 'utf8');
 const nums: number[][] = file
-  .split("\n")
-  .map((lines) => lines.split("").map((x) => parseInt(x, 10)));
+    .split('\n')
+    .map((lines) => lines.split('').map((x) => parseInt(x, 10)));
 
 const leftBounds: number[] = nums.map((_) => 0);
 const rightBounds: number[] = nums.map((_) => 12);
 const temps: number[] = nums.map((_) => 0);
 
-// console.log(nums, leftBounds, rightBounds, temps);
-
 nums.forEach((line, index) => {
-  console.log(line);
-  while (rightBounds[index]) {
-    // console.log("right", rightBounds[index]);
-    // console.log("left", leftBounds[index]);
-    const sliceRight = -rightBounds[index] + 1;
-    const sliced = line.slice(
-      leftBounds[index],
-      sliceRight !== 0 ? sliceRight : undefined
-    );
-    // console.log("sliced", sliced);
-    const max = sliced.reduce((pV, aV) => (pV > aV ? pV : aV));
-    const pos = sliced.indexOf(max);
-    leftBounds[index] = (leftBounds[index] as number) + pos + 1;
-    temps[index] =
-      (temps[index] as number) + max * 10 ** (rightBounds[index] - 1);
-    // console.log("max", max, "pos", pos);
-    // console.log("temp", temps[index]);
-    rightBounds[index]--;
-  }
+    while (rightBounds[index]) {
+        const sliceRight = -rightBounds[index] + 1;
+        const sliced = line.slice(
+            leftBounds[index],
+            sliceRight !== 0 ? sliceRight : undefined,
+        );
+        const max = sliced.reduce((pV, aV) => (pV > aV ? pV : aV));
+        const pos = sliced.indexOf(max);
+        leftBounds[index] = (leftBounds[index] as number) + pos + 1;
+        temps[index] =
+            (temps[index] as number) + max * 10 ** (rightBounds[index] - 1);
+        rightBounds[index]--;
+    }
 });
 
-console.log(temps);
 console.log(temps.reduce((a, b) => a + b));
