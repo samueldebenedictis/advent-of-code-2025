2ed1c9355af0311a040f798b0072776bcb9df5a5
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-12-09 13:11:09 +0100

    add day 9 part 1

---
+++ src/9/9.0.ts
@@ -0,0 +1,27 @@
+import * as fs from 'node:fs';
+
+type Tile = { id: number; x: number; y: number };
+const tiles: Tile[] = [];
+
+const file = fs.readFileSync('src/9/input.txt', 'utf8');
+
+const line = file.split('\n');
+line.forEach((value, index) => {
+    const [x, y] = value.split(',').map((el) => parseInt(el, 10));
+    tiles.push({ id: index, x: x as number, y: y as number });
+});
+
+let maxArea = 0;
+
+for (const tile1 of tiles) {
+    for (const tile2 of tiles) {
+        const area =
+            (Math.abs(tile1.x - tile2.x) + 1) *
+            (Math.abs(tile1.y - tile2.y) + 1);
+        if (area > maxArea) {
+            maxArea = area;
+        }
+    }
+}
+
+console.log(maxArea);
